b2ffdfaba0ffe3144410a65e803010a8918625c6
Tony Cook <tony@develop-help.com>
2001-05-08 03:19:32 +0000

    only regen if the user is allowed to

--- site/cgi-bin/admin/generate.pl
+++ site/cgi-bin/admin/generate.pl
@@ -5,18 +5,21 @@ use Articles;
 use CGI qw(:standard);
 use CGI::Carp qw(fatalsToBrowser);
 use Constants qw($URLBASE);
+use Util qw(generate_button);
 
 my $articles = Articles->new;
-my $id = param('id');
-if (defined $id) {
-  use Util 'generate_article';
-  my $article = $articles->getByPkey($id)
-    or die "No such article $id found";
-  generate_article($articles, $article);
-}
-else {
-  use Util 'generate_all';
-  generate_all($articles);
+  my $id = param('id');
+if (generate_button()) {
+  if (defined $id) {
+    use Util 'generate_article';
+    my $article = $articles->getByPkey($id)
+      or die "No such article $id found";
+    generate_article($articles, $article);
+  }
+  else {
+    use Util 'generate_all';
+    generate_all($articles);
+  }
 }
 
 my $fromid = param('fromid') || $id;
