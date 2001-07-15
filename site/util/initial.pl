0629d909b01173f1ebb1a0c269fa27cdf314138f
Tony Cook <tony@develop-help.com>
2001-07-15 04:29:05 +0000

    new default articles

--- site/util/initial.pl
+++ site/util/initial.pl
@@ -108,6 +108,168 @@ my @prebuilt =
     level=>2,
     listed=>2,
    },
+   {
+    id=>5,
+    parentid=>1,
+    displayOrder=>10000,
+    title=>'[sidebar subsection]',
+    titleImage=>'',
+    body=>'', # don't set this
+    imagePos=>'tr',
+    release=>'0000-00-00 00:00:00',
+    expire=>'9999-12-31 23:59:59',
+    lastModified=>'2000-11-27 14:00:00',
+    keyword=>'',
+    template=>'sidebar_section.tmpl',
+    link=>'',
+    admin=>$CGI_URI.'/admin/add.pl?id=5',
+    threshold=>1000, # ignored
+    summaryLength=>1000, #ignored
+    generator=>'Generate::Article',
+    thumbImage=>'',
+    thumbWidth=>0,
+    thumbHeight=>0,
+    level=>2,
+    listed=>0,
+   },
+   {
+    id=>6,
+    parentid=>2,
+    displayOrder=>10000,
+    title=>'[formatting guide]',
+    titleImage=>'',
+    body=><<EOS,
+anchor[basic] b[Sample basic formatting:]
+indent[link[/a/8.html|regular link text]
+
+b[link[/a/8.html|bold link text]]
+
+i[link[/a/8.html|italic link text]]
+
+i[b[link[/a/8.html|bold italic link text]]]
+
+link[http://www.visualthought.com.au|link to external web site]
+]
+
+align[center|Align center text (NB: American spelling)]
+
+align[right|Align right text]
+
+This is how you can make an email link: link[mailto:adriann@visualthought.com.au|email us here!]
+
+hrcolor[100%|1|#ffffff] anchor[font]
+b[Sample font sizing:] (use numbers 1 - 7 only)
+indent[fontcolor[7|#6699CC|This is font size 7 and colour]
+
+font[6|This is font size 6]
+
+font[5|This is font size 5]
+
+font[4|This is font size 4]
+
+font[3|This is font size 3]
+
+font[2|This is font size 2]
+
+font[1|This is font size 1]
+
+(but they can be minus numbers too)
+
+font[-2|This is font size -2]
+
+font[-3|This is font size -3]
+
+(or positive numbers)
+
+font[+1|This is font size +1]
+
+]
+
+b[HINT:] The default font size for your body text is size 2 and the
+headings are size 4 Bold.
+
+hrcolor[100%|1|#ffffff] anchor[indent]
+b[Sample indenting and bullet points:]
+indent[This is a simple indent and bullet list
+** Add a bullet point to this line
+** Hit enter and type another
+** And now one final bullet
+
+## And this is a numbered list
+## It's very easy to format lists now
+## Now we can count to three]
+
+hrcolor[100%|1|#ffffff]
+b[The NSWFITC colours:]
+indent[
+table[bgcolor=#ffffff width=170 cellpadding=5
+|fontcolor[2|#000000|White - #FFFFFF]
+]
+table[bgcolor=#6699CC width=170 cellpadding=5
+|fontcolor[2|#000000|Light Blue - #6699CC]
+]
+table[bgcolor=#336699 width=170 cellpadding=5
+|fontcolor[2|#ffffff|Medium Blue - #336699]
+]
+table[bgcolor=#336699 width=170 cellpadding=1 cellspacing=0 |table[bgcolor=#003366 width=100% cellspacing=0 cellpadding=5
+|fontcolor[2|#ffffff|Regular Blue - #003366]
+]]
+table[bgcolor=#000033 width=170 cellpadding=5
+|fontcolor[2|#ffffff|Dark Blue - #000033]
+]
+]
+hrcolor[100%|1|#ffffff]
+b[Sample links to an anchor:]
+indent[link[#basic|Jump to "Sample basic formatting"]
+link[#font|Jump to "Sample font sizing"]
+r link[#indent|Jump to "Sample indenting and bullet points"]
+link[#table|Jump to "Making a table"]]
+
+
+hrcolor[100%|1|#ffffff] anchor[table]
+b[Making a table:]
+indent[
+BSE has special tags to create simple tables, but if you wish to do anything more complicated, we recommend you develop the table in HTML and insert the raw HTML into the body text field.
+
+table[
+|a very|simple
+|table|with 4 cells
+]
+
+table[bgcolor=#6699CC width=90% cellpadding=5
+|align[center|link[/a/2.html|this one]]|is
+|not so|simple
+]
+
+
+table[width=100% cellpadding=5
+|b[font[2|Project Management]]|font[2|Project management services for buildings, infrastructure, technology and environment, relocations.]
+|b[font[2|Events]]|font[2|Event marketing and management, overlay works, venue management.]
+|b[font[2|Property]]|font[2|Property development management and advisory.] 
+|b[font[2|Management Consulting]]|font[2|Business improvement, change management, market testing, outsourcing, contracts and relationships, industrial relations, safety, asset management.]
+|b[font[2|Quality Assurance]]|font[2|Independent 3rd party audits, quality systems, certification, OHS&R programmes.]
+]
+]
+
+hrcolor[100%|1|#ffffff]
+EOS
+    imagePos=>'tr',
+    release=>'0000-00-00 00:00:00',
+    expire=>'9999-12-31 23:59:59',
+    lastModified=>'2000-11-27 14:00:00',
+    keyword=>'',
+    template=>'sidebar_section.tmpl',
+    link=>'',
+    admin=>$CGI_URI.'/admin/add.pl?id=5',
+    threshold=>1000, # ignored
+    summaryLength=>1000, #ignored
+    generator=>'Generate::Article',
+    thumbImage=>'',
+    thumbWidth=>0,
+    thumbHeight=>0,
+    level=>2,
+    listed=>0,
+   },
   );
 
 my $dbh = DBI->connect("dbi:$DBD:$DB", $UN, $PW)
