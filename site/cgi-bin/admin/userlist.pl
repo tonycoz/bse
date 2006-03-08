e36c2d8537bd2a3a7e84dc5adfff79625481bf65
Tony Cook <tony@develop-help.com>
2006-03-08 04:00:30 +0000

    userlist.pl can now dump a range of logons, specified with minlogon and maxlogon.  If the field is missing or empty then the range includes the first or last by logon as appropriate.

--- site/cgi-bin/admin/userlist.pl
+++ site/cgi-bin/admin/userlist.pl
@@ -10,7 +10,10 @@ my $req = BSE::Request->new;
 
 if (BSE::Permissions->check_logon($req)) {
   if ($req->user_can('bse_siteuser_export')) {
-    do_dump();
+    my $cgi = $req->cgi;
+    my $min_logon = $cgi->param('minlogon');
+    my $max_logon = $cgi->param('maxlogon');
+    do_dump($min_logon, $max_logon);
   }
   else {
     refresh_to($req->url('menu', { m => 'Access denied' }));
@@ -21,12 +24,36 @@ else {
 }
 
 sub do_dump {
+  my ($min, $max) = @_;
+
   my $dh = single BSE::DB;
+
+  my $sql;
+  my @parms;
+  if (defined $min and length $min) {
+    if (defined $max and length $max) {
+      $sql = 'select * from site_users where userId >= ? and userId < ?';
+      @parms = ( $min, $max );
+    }
+    else {
+      $sql = 'select * from site_users where userId >= ?';
+      @parms = ( $min );
+    }
+  }
+  else {
+    if (defined $max and length $max) {
+      $sql = 'select * from site_users where userId <= ?';
+      @parms = ( $max );
+    }
+    else {
+      $sql = 'select * from site_users';
+    }
+  }
   
-  my $sth = $dh->{dbh}->prepare('select * from site_users')
+  my $sth = $dh->{dbh}->prepare($sql)
     or die "Cannot prepare site_users select:", $DBI::errstr;
   
-  $sth->execute()
+  $sth->execute(@parms)
     or die "Cannot execute site_users select:", $sth->errstr;
   
   print "Content-Type: application/octet-stream\n";
