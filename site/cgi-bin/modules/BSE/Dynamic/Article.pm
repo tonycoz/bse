e0ed6370ca1a2a082593a4ee10d310011bcb114a
Tony Cook <tony@develop-help.com>
2006-08-14 06:36:06 +0000

    added dynmoveallkid tag for use on dynamic pages.

--- site/cgi-bin/modules/BSE/Dynamic/Article.pm
+++ site/cgi-bin/modules/BSE/Dynamic/Article.pm
@@ -38,14 +38,20 @@ sub tags {
   my ($self, $article) = @_;
   
   $self->{req}->set_article(dynarticle => $article);
+  my $allkid_index;
+  my $allkid_data;
   return
     (
      $self->SUPER::tags(),
      dynarticle => [ \&tag_hash, $article ],
      ifAncestor => [ tag_ifAncestor => $self, $article ],
-     $self->dyn_iterator('dynallkids', 'dynallkid', $article),
+     $self->dyn_iterator('dynallkids', 'dynallkid', $article,
+			 \$allkid_index, \$allkid_data),
      $self->dyn_iterator('dynchildren', 'dynchild', $article),
      $self->dyn_iterator('dynstepparents', 'dynstepparent', $article),
+     dynmoveallkid => 
+     [ tag_dynmove => $self, \$allkid_index, \$allkid_data, 
+       "stepparent=$article->{id}" ],
     );
 }
 
@@ -108,10 +114,47 @@ sub tag_ifAncestor {
   return 0;
 }
 
+sub tag_dynmove {
+  my ($self, $rindex, $rrdata, $url_prefix, $args, $acts, $templater) = @_;
+
+  return '' unless $self->admin_mode;
+
+  return '' unless $$rrdata && @$$rrdata > 1;
+
+  require BSE::Arrows;
+  *make_arrows = \&BSE::Arrows::make_arrows;
+
+  my ($img_prefix, $url_add) = 
+    DevHelp::Tags->get_parms($args, $acts, $templater);
+  defined $img_prefix or $img_prefix = '';
+  defined $url_add or $url_add = '';
+  my $refresh_to = $ENV{SCRIPT_NAME} . "?id=" . 
+    $self->{req}->get_article('dynarticle')->{id} . $url_add;
+  my $move = "$Constants::CGI_URI/admin/move.pl?";
+  $move .= $url_prefix . '&' if $url_prefix;
+  $move .= 'd=swap&id=' . $$$rrdata[$$rindex]{id} . '&';
+  my $down_url = '';
+  if ($$rindex < $#$$rrdata) {
+    $down_url = $move . 'other=' . $$$rrdata[$$rindex+1]{id};
+  }
+  my $up_url = '';
+  if ($$rindex > 0) {
+    $up_url = $move . 'other=' . $$$rrdata[$$rindex-1]{id};
+  }
+
+  return make_arrows($self->{req}->cfg, $down_url, $up_url, $refresh_to, $img_prefix);
+}
+
 sub get_real_article {
   my ($self, $article) = @_;
 
   return $article;
+}
+
+sub admin_mode {
+  my ($self) = @_;
+
+  $self->{admin};
 }
 
 1;
