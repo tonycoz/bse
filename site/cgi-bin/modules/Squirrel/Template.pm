2c3726e0d9347724f0dc55b715c62431c3b297df
Tony Cook <tony@develop-help.com>
2007-09-12 03:37:00 +0000

    reposition the localize

--- site/cgi-bin/modules/Squirrel/Template.pm
+++ site/cgi-bin/modules/Squirrel/Template.pm
@@ -221,9 +221,9 @@ sub cond {
   defined $args or $args = '';
   print STDERR "cond $name $args\n" if DEBUG;
 
+  local $SIG{__DIE__};
   my $result =
     eval {
-      local $SIG{__DIE__};
       if (exists $acts->{"if$name"}) {
 	print STDERR " found cond if$name\n" if DEBUG > 1;
 	my $cond = $self->low_perform($acts, "if$name", $args, '');
