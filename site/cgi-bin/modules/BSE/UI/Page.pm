e7367b88ca16fa1599d93fa740b12adbbeb1bf0f
Tony Cook <tony@develop-help.com>
2008-02-20 04:04:05 +0000

    add entries for each UI handler to the nuser section make the page UI handler work with nuser.pl

--- site/cgi-bin/modules/BSE/UI/Page.pm
+++ site/cgi-bin/modules/BSE/UI/Page.pm
@@ -2,41 +2,57 @@ package BSE::UI::Page;
 use strict;
 use Articles;
 use DevHelp::HTML qw(escape_uri);
+use BSE::UI::Dispatch;
+our @ISA = qw(BSE::UI::Dispatch);
 
 # we don't do anything fancy on dispatch yet, so don't use the 
 # dispatch classes
 sub dispatch {
-  my ($class, $req) = @_;
+  my ($self, $req) = @_;
 
   my $cgi = $req->cgi;
   my $cfg = $req->cfg;
-  my $id = $cgi->param('page');
   my $article;
-  my $prefix = $cfg->entry('basic', 'alias_prefix', '');
-  my @more_headers;
-  if ($id) {
-    $id && $id =~ /^\d+$/
-      or return $class->error($req, "page parameter not valid");
-    $article = Articles->getByPkey($id)
-      or return $class->error($req, "unknown article id $id");
-  }
-  elsif (my $alias = $cgi->param('alias')) {
-    $article = Articles->getBy(linkAlias => $alias)
-      or return $class->error($req, "Unknown article alias '$alias'");
-  }
-  elsif ($ENV{REDIRECT_URL} && $ENV{SCRIPT_URL} =~ m(^\Q$prefix\E/)) {
-    (my $url = $ENV{SCRIPT_URL}) =~ s(^\Q$prefix\E/)();
-    my ($alias) = $url =~ /^([a-zA-Z0-9_]+)/
-      or return $class->error($req, "Missing document $ENV{SCRIPT_URL}");
-
-    $article = Articles->getBy(linkAlias => $alias)
-      or return $class->error($req, "Unknown article alias '$alias'");
-
-    # have the client treat this as successful, though an error is
-    # still written to the Apache error log
-    push @more_headers, "Status: 200";
+  my $id;
+    my @more_headers;
+  if ($self->action) {
+    my $action = $self->action;
+    if ($action =~ /^\d+$/) {
+      $article = Articles->getByPkey($action)
+	or return $self->error($req, "unknown article id $action");
+    }
+    elsif ($action =~ /^\w+$/) {
+      $article = Articles->getBy(linkAlias => $action)
+	or return $self->error($req, "Unknown article alias '$action'");
+    }
   }
   else {
+    $id = $cgi->param('page');
+    my $prefix = $cfg->entry('basic', 'alias_prefix', '');
+    if ($id) {
+      $id && $id =~ /^\d+$/
+	or return $self->error($req, "page parameter not valid");
+      $article = Articles->getByPkey($id)
+	or return $self->error($req, "unknown article id $id");
+    }
+    elsif (my $alias = $cgi->param('alias')) {
+      $article = Articles->getBy(linkAlias => $alias)
+	or return $self->error($req, "Unknown article alias '$alias'");
+    }
+    elsif ($ENV{REDIRECT_URL} && $ENV{SCRIPT_URL} =~ m(^\Q$prefix\E/)) {
+      (my $url = $ENV{SCRIPT_URL}) =~ s(^\Q$prefix\E/)();
+      my ($alias) = $url =~ /^([a-zA-Z0-9_]+)/
+	or return $self->error($req, "Missing document $ENV{SCRIPT_URL}");
+      
+      $article = Articles->getBy(linkAlias => $alias)
+	or return $self->error($req, "Unknown article alias '$alias'");
+      
+      # have the client treat this as successful, though an error is
+      # still written to the Apache error log
+      push @more_headers, "Status: 200";
+    }
+  }
+  unless ($article) {
     if ($cfg->entry('debug', 'nopage')) {
       print STDERR "Request to page.pl with no page or alias - ";
       if ($ENV{HTTP_REFERER}) {
@@ -46,8 +62,9 @@ sub dispatch {
 	print STDERR "No referer\n";
       }
     }
-    return $class->error($req, "No page or page alias specified for display");
+    return $self->error($req, "No page or page alias specified for display");
   }
+
   $id = $article->{id};
 
   if (!$article->is_dynamic 
@@ -78,17 +95,17 @@ sub dispatch {
   eval {
     require $gen_file;
   };
-  $@ and return $class->error($req, $@);
+  $@ and return $self->error($req, $@);
   my $gen = $gen_class->new($req);
   $article = $gen->get_real_article($article)
-    or return $class->error($req, "Cannot get full article for $id");
+    or return $self->error($req, "Cannot get full article for $id");
 
   $article->is_dynamic
     or print STDERR "** page.pl called for non-dynamic article $id\n";
 
   unless ($req->siteuser_has_access($article)) {
     if ($req->siteuser) {
-      return $class->error
+      return $self->error
 	($req, "You do not have access to view this article");
     }
     else {
@@ -118,7 +135,7 @@ sub dispatch {
     $debug_jit && !$dynamic_pregen
       and print STDERR "** JIT: $id - pregen page not found but JIT off\n";
     
-    $template = $class->_generate_pregen($req, $article, $srcname);
+    $template = $self->_generate_pregen($req, $article, $srcname);
   }
 
   my $result = $gen->generate($article, $template);
@@ -130,16 +147,8 @@ sub dispatch {
   return $result;
 }
 
-# returns an error page
-sub error {
-  my ($class, $req, $msg) = @_;
-
-  require BSE::UI::Dispatch;
-  return BSE::UI::Dispatch->error($req, { error => $msg });
-}
-
 sub _generate_pregen {
-  my ($class, $req, $article, $srcname) = @_;
+  my ($self, $req, $article, $srcname) = @_;
 
   my $articles = 'Articles';
   my $genname = $article->{generator};
