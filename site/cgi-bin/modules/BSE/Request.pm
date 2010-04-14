aebf12bc41ffa5cdbdf6e11ed3f79a747e1f4483
Tony Cook <tony@develop-help.com>
2010-04-14 12:43:28 +0000

    don't make a session if the caller asks us not to (performance, locking)

--- site/cgi-bin/modules/BSE/Request.pm
+++ site/cgi-bin/modules/BSE/Request.pm
@@ -8,9 +8,11 @@ sub new {
 
   my $self = $class->SUPER::new(%opts);
 
-  my %session;
-  BSE::Session->tie_it(\%session, $self->{cfg});
-  $self->{session} = \%session;
+  unless ($opts{nosession}) {
+    my %session;
+    BSE::Session->tie_it(\%session, $self->{cfg});
+    $self->{session} = \%session;
+  }
   $self->{cache_stats} = $self->{cfg}->entry('debug', 'cache_stats', 0);
   if ($self->{cache_stats}) {
     @{$self}{qw/siteuser_calls siteuser_cached has_access_cached has_access_total/} = ( 0, 0, 0, 0 );
