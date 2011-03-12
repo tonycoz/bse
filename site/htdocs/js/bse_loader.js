ff801f576e53ac847b126c90e460e01ceb6b1275
Tony Cook <tony@develop-help.com>
2011-03-13 10:33:16 +1100

    split out the loader class

---
+++ site/htdocs/js/bse_loader.js
@@ -0,0 +1,36 @@
+var BSELoader = Class.create({
+  initialize: function(options) {
+    this.options = Object.extend({}, options);
+    this._scripts = options.scripts.clone();
+    this._load_next_script();
+  },
+  _load_next_script: function() {
+    var uri = this._scripts.shift();
+    var scr = new Element("script", { src: uri, type: "text/javascript" });
+    scr.loadDone = false;
+    scr.onload = function(scr) {
+      if (!this.loadDone) {
+	scr.loadDone = true;
+	this._script_loaded();
+      }
+    }.bind(this, scr);
+    scr.onreadystatechange = function(scr) {
+      if ((scr.readyState === "loaded" || scr.readyState === "complete")
+	  && !scr.loadDone) {
+	scr.loadDone = true;
+	this._script_loaded();
+      }
+    }.bind(this, scr);
+    var head = $$("head")[0];
+    head.appendChild(scr);
+  },
+  _script_loaded: function() {
+    if (this._scripts.length) {
+      this._load_next_script();
+    }
+    else {
+      if (this.options.onLoaded != null)
+	this.options.onLoaded();
+    }
+  }
+});
