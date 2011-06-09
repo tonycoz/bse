9dfb6c2055f1daa0a0b50803947620bacf23f2c1
Tony Cook <tony@develop-help.com>
2011-06-09 15:44:16 +1000

    call price in scalar context when building cart item hash

--- site/cgi-bin/modules/BSE/UI/Shop.pm
+++ site/cgi-bin/modules/BSE/UI/Shop.pm
@@ -17,7 +17,7 @@ use BSE::Shipping;
 use BSE::Countries qw(bse_country_code);
 use BSE::Util::Secure qw(make_secret);
 
-our $VERSION = "1.013";
+our $VERSION = "1.014";
 
 use constant MSG_SHOP_CART_FULL => 'Your shopping cart is full, please remove an item and try adding an item again';
 
@@ -204,7 +204,7 @@ sub req_add {
       { 
        productId => $product->{id}, 
        units => $quantity, 
-       price=>$product->price(user => scalar $req->siteuser),
+       price=> scalar $product->price(user => scalar $req->siteuser),
        options=>$options,
        %$extras,
       };
@@ -273,7 +273,7 @@ sub req_addsingle {
       { 
        productId => $addid, 
        units => $quantity, 
-       price=>$product->price(user => scalar $req->siteuser),
+       price=> scalar $product->price(user => scalar $req->siteuser),
        options=>$options,
        %$extras,
       };
@@ -401,7 +401,7 @@ sub req_addmultiple {
 	{ 
 	 productId => $product->{id},
 	 units => $addition->{quantity}, 
-	 price=>$product->price(user => scalar $req->siteuser),
+	 price=> scalar $product->price(user => scalar $req->siteuser),
 	 options=>[],
 	 %{$addition->{extras}},
 	};
