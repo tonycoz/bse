f196512602243f7d3b572d59152a56d6e81e26ef
Tony Cook <tony@develop-help.com>
2011-06-12 12:50:03 +1000

    use the new error handling setup for user.pl

--- site/cgi-bin/user.pl
+++ site/cgi-bin/user.pl
@@ -4,20 +4,6 @@ BEGIN { $ENV{DISPLAY} = '192.168.32.54:0.0'; }
 use strict;
 use FindBin;
 use lib "$FindBin::Bin/modules";
-use Constants;
-use BSE::UserReg;
-use BSE::Request;
-use Carp 'confess';
+use BSE::UI;
 
-$SIG{__DIE__} = sub { confess $@ };
-
-my $req = BSE::Request->new;
-my $result = BSE::UserReg->new->dispatch($req);
-
-# just for now, eventually all UserReg methods should properly return
-# a result
-if ($result) {
-  my $cfg = $req->cfg;
-  undef $req;
-  BSE::Template->output_resultc($cfg, $result);
-}
+BSE::UI->run("BSE::UserReg");
