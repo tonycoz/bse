941407346d51e990d2e3ccb90f5c787ff59f914a
Tony Cook <tony@develop-help.com>
2011-06-15 16:21:03 +1000

    Revert "use the new error handling setup for user.pl"
    This reverts commit f196512602243f7d3b572d59152a56d6e81e26ef.

--- site/cgi-bin/user.pl
+++ site/cgi-bin/user.pl
@@ -4,6 +4,20 @@ BEGIN { $ENV{DISPLAY} = '192.168.32.54:0.0'; }
 use strict;
 use FindBin;
 use lib "$FindBin::Bin/modules";
-use BSE::UI;
+use Constants;
+use BSE::UserReg;
+use BSE::Request;
+use Carp 'confess';
 
-BSE::UI->run("BSE::UserReg");
+$SIG{__DIE__} = sub { confess $@ };
+
+my $req = BSE::Request->new;
+my $result = BSE::UserReg->new->dispatch($req);
+
+# just for now, eventually all UserReg methods should properly return
+# a result
+if ($result) {
+  my $cfg = $req->cfg;
+  undef $req;
+  BSE::Template->output_resultc($cfg, $result);
+}
