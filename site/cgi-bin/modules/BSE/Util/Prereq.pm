98a44f213723cc6cc1d70d812668158a6b16e3f1
Tony Cook <tony@develop-help.com>
2015-07-17 16:46:18 +1000

    update pre-requisites

--- site/cgi-bin/modules/BSE/Util/Prereq.pm
+++ site/cgi-bin/modules/BSE/Util/Prereq.pm
@@ -1,13 +1,16 @@
 package BSE::Util::Prereq;
 use strict;
 
-our $VERSION = "1.002";
+our $VERSION = "1.003";
 
 # pre-requisites for various modules
 
 our %prereqs =
   (
+   "BSE::Cache::CHI" => [ "CHI" ],
    "BSE::Cache::Memcached" => [ "Cache::Memcached::Fast" ],
+   "BSE::Importer::Source::XLS" => [ "Spreadsheet::ParseExcel" ],
+   "BSE::Importer::Source::CSV" => [ "Text::CSV" ],
    "BSE::Storage::AmazonS3" => [ "Net::Amazon::S3" ],
    "BSE::Util::ValidateHTML::Tidy" => [ "HTML::Tidy" ],
    "BSE::Util::ValidateHTML::W3C" => [ "WebService::Validator::HTML::W3C", "XML::XPath" ],
@@ -15,4 +18,5 @@ our %prereqs =
    "Courier::Fastway::Road" => [ "XML::Parser" ],
    "Courier::Fastway::Satchel" => [ "XML::Parser" ],
    "DevHelp::Payments::SecurePayXML" => [ "XML::Simple" ],
+   "DevHelp::Payments::Eway" => [ "XML::LibXML" ],
   );
